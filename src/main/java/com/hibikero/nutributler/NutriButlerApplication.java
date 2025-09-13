fb7f2e6741c6940a0ce65fb8f75e5a009277dc1d
HibiKero <2169299483@qq.com>
2025-09-13 13:13:24 +0800

    立项

---
+++ src/main/java/com/hibikero/nutributler/NutriButlerApplication.java
@@ -0,0 +1,13 @@
+package com.hibikero.nutributler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriButlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutriButlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hibikero/nutributler/NutriButlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibikero.nutributler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriButlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
